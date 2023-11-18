765748a8f90ab5b83ef1355b405e3cffcd1a04e5
Robin Opletal <me@robinopletal.com>
2023-11-18 12:51:10 +0100

    feat(run-playbook): add upgrade option via env variable

--- run-playbook.sh
+++ run-playbook.sh
@@ -1,1 +1,7 @@
-ansible-playbook -i inventory.ini -u root playbook.yml --vault-password-file=~/.ansible/personal_secret.txt
+#!/usr/bin/env sh
+
+if [ upgrade ]; then
+    UPGRADE="-e upgrade=true";
+fi
+
+ansible-playbook -i inventory.ini -u root playbook.yml --vault-password-file=~/.ansible/personal_secret.txt $UPGRADE
